03a646bf068b56d4b80ec93447cd10cb59741c57
Erza Kaneki <nevin.kadlec@gmail.com>
2024-08-30 10:26:01 -0500

    Added find (n)th node example.

--- Nodes.py
+++ Nodes.py
@@ -102,54 +102,81 @@
 #                   IF VAL1 == VAL2: -> PRINT WARNING -> RETURN  DON'T SWAP SAME VAL'S POINTLESS RUN TIME
 
 
-#SWAPPING NODE EXAMPLE
-def swap_nodes(input_list, val1, val2):
-  print(f'Swapping {val1} with {val2}')
+#SWAPPING NODE EXAMPLE  SWAPS 9 WITH 5
+# def swap_nodes(input_list, val1, val2):
+#   print(f'Swapping {val1} with {val2}')
 
-  node1_prev = None
-  node2_prev = None
-  node1 = input_list.head_node
-  node2 = input_list.head_node
+#   node1_prev = None
+#   node2_prev = None
+#   node1 = input_list.head_node
+#   node2 = input_list.head_node
 
-  if val1 == val2:
-    print("Elements are the same - no swap needed")
-    return
+#   if val1 == val2:
+#     print("Elements are the same - no swap needed")
+#     return
 
-  while node1 is not None:
-    if node1.get_value() == val1:
-      break
-    node1_prev = node1
-    node1 = node1.get_next_node()
+#   while node1 is not None:
+#     if node1.get_value() == val1:
+#       break
+#     node1_prev = node1
+#     node1 = node1.get_next_node()
 
-  while node2 is not None:
-    if node2.get_value() == val2:
-      break
-    node2_prev = node2
-    node2 = node2.get_next_node()
+#   while node2 is not None:
+#     if node2.get_value() == val2:
+#       break
+#     node2_prev = node2
+#     node2 = node2.get_next_node()
 
-  if (node1 is None or node2 is None):
-    print("Swap not possible - one or more element is not in the list")
-    return
+#   if (node1 is None or node2 is None):
+#     print("Swap not possible - one or more element is not in the list")
+#     return
 
-  if node1_prev is None:
-    input_list.head_node = node2
-  else:
-    node1_prev.set_next_node(node2)
+#   if node1_prev is None:
+#     input_list.head_node = node2
+#   else:
+#     node1_prev.set_next_node(node2)
 
-  if node2_prev is None:
-    input_list.head_node = node1
-  else:
-    node2_prev.set_next_node(node1)
+#   if node2_prev is None:
+#     input_list.head_node = node1
+#   else:
+#     node2_prev.set_next_node(node1)
 
-  temp = node1.get_next_node()
-  node1.set_next_node(node2.get_next_node())
-  node2.set_next_node(temp)
+#   temp = node1.get_next_node()
+#   node1.set_next_node(node2.get_next_node())
+#   node2.set_next_node(temp)
 
 
-ll = LinkedList.LinkedList()
-for i in range(10):
-  ll.insert_beginning(i)
+# ll = LinkedList.LinkedList()
+# for i in range(10):
+#   ll.insert_beginning(i)
 
-print(ll.stringify_list())
-swap_nodes(ll, 9, 5)
-print(ll.stringify_list())
+# print(ll.stringify_list())
+# swap_nodes(ll, 9, 5)
+# print(ll.stringify_list())
+
+
+#FINDING nTH NODE WITH 2 RUNNERS
+# def nth_last_node(linked_list, n):
+#   claimer = None
+#   explorer = linked_list.head_node
+#   count =1
+#   while explorer:
+#     explorer = explorer.get_next_node()
+#     count += 1
+#     if count >= n + 1:
+#       if claimer is None:
+#         claimer = linked_list.head_node
+#       else:
+#         claimer = claimer.get_next_node()
+#   return claimer
+
+# def generate_test_linked_list():
+#   linked_list = LinkedList()
+#   for i in range(50, 0, -1):
+#     linked_list.insert_beginning(i)
+#   return linked_list
+
+# test_list = generate_test_linked_list()
+# print(test_list.stringify_list())
+# nth_last = nth_last_node(test_list, 4)
+# print(nth_last.value)
