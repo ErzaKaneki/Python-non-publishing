bca76afe07c83a935e5811d89f38bc2a41f4d0be
Erza Kaneki <nevin.kadlec@gmail.com>
2024-08-30 09:17:48 -0500

    commented out Implimentation.

--- Nodes.py
+++ Nodes.py
@@ -28,63 +28,65 @@
 
 
 #NODE IMPLEMENTATION
-class Node:
-    def __init__(self, value, next_node = None):
-        self.value = value
-        self.next_node = next_node
+# class Node:
+#     def __init__(self, value, next_node = None):
+#         self.value = value
+#         self.next_node = next_node
 
-    def get_value(self):
-        return self.value
+#     def get_value(self):
+#         return self.value
     
-    def get_next_node(self):
-        return self.next_node
+#     def get_next_node(self):
+#         return self.next_node
 
-    def set_next_node(self, next_node):
-        self.next_node = next_node
-        return self.next_node
+#     def set_next_node(self, next_node):
+#         self.next_node = next_node
+#         return self.next_node
     
 
-class LinkedList:
+# class LinkedList:
 
-    def __init__(self, value = None):
-        self.head_node = Node(value)
+#     def __init__(self, value = None):
+#         self.head_node = Node(value)
 
-    def get_head_node(self):
-        return self.head_node
+#     def get_head_node(self):
+#         return self.head_node
     
-    def insert_beginning(self, new_value):
-        new_node = Node(new_value)
-        new_node.set_next_node(self.head_node)
-        self.head_node = new_node
+#     def insert_beginning(self, new_value):
+#         new_node = Node(new_value)
+#         new_node.set_next_node(self.head_node)
+#         self.head_node = new_node
     
-    def stringify_list(self):
-        end_string = ""
-        current_node = self.get_head_node()
-        while current_node:
-            if current_node.get_value() != None:
-                end_string += str(current_node.get_value()) + "\n"
-            current_node = current_node.get_next_node()
-        return end_string
+#     def stringify_list(self):
+#         end_string = ""
+#         current_node = self.get_head_node()
+#         while current_node:
+#             if current_node.get_value() != None:
+#                 end_string += str(current_node.get_value()) + "\n"
+#             current_node = current_node.get_next_node()
+#         return end_string
 
-    def remove_node(self, value_to_remove):
-        current_node = self.head_node
-        if current_node.get_value() == value_to_remove:
-            self.head_node = self.head_node.get_next_node()
-            current_node = self.head_node
-        else:
-            while current_node:
-                previous_node = current_node
-                current_node = current_node.next_node
-                if current_node.get_value() == value_to_remove:
-                    previous_node.next_node = current_node.next_node  #OR-> current_node.set_next_node(next_node.get_next_node()) <- TRANSPARENCY
-                    current_node = None
+#     def remove_node(self, value_to_remove):
+#         current_node = self.head_node
+#         if current_node.get_value() == value_to_remove:
+#             self.head_node = self.head_node.get_next_node()
+#             current_node = self.head_node
+#         else:
+#             while current_node:
+#                 previous_node = current_node
+#                 current_node = current_node.next_node
+#                 if current_node.get_value() == value_to_remove:
+#                     previous_node.next_node = current_node.next_node  #OR-> current_node.set_next_node(next_node.get_next_node()) <- TRANSPARENCY
+#                     current_node = None
 
-LL = LinkedList()
-LL.insert_beginning(5)
-LL.insert_beginning(52)
-LL.insert_beginning(3)
-LL.insert_beginning(9)
-LL.insert_beginning(3)
-LL.insert_beginning(7)
-LL.remove_node(3)        
-print(LL.stringify_list())
+# LL = LinkedList()
+# LL.insert_beginning(5)
+# LL.insert_beginning(52)
+# LL.insert_beginning(3)
+# LL.insert_beginning(9)
+# LL.insert_beginning(3)
+# LL.insert_beginning(7)
+# LL.remove_node(3)        
+# print(LL.stringify_list())
+
+
