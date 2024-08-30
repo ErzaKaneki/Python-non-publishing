7d280f7ed2218883533fc93cfebf9d1a88a30600
Erza Kaneki <nevin.kadlec@gmail.com>
2024-08-30 10:01:41 -0500

    Added swapping node tips, and an example of a swapping module.

--- Nodes.py
+++ Nodes.py
@@ -1,3 +1,4 @@
+#NODES    XD
 # class Node:
 #     def __init__(self, value, link_node = None):
 #         self.value = value
@@ -90,3 +91,65 @@
 # print(LL.stringify_list())
 
 
+#SWAPPING TWO NODES PREV_NODE1 & NODE1 SEARCH FOR FIRST NODE
+#                   PREV_NODE2 & NODE2 SEARCH FOR OTHER NODE
+#                   IF EITHER NODE PREV_NODE = NONE MEANS HEAD SO MAKE THE SWAP TO THE HEAD
+#                   OTHERWISE PREV_NODE1.NEXT_NODE = NODE2
+#                         AND NODE1.NEXT_NODE = NODE2.NEXT_NODE
+#                   ALSO PREV_NODE2.NEXT_NODE = NODE1
+#                         AND NODE2.NEXT_NODE = NODE1.NEXT_NODE
+#  REMEMBER ->      IF NODE1 IS NONE OR NODE2 IS NONE  CAN'T SWAP
+#                   IF VAL1 == VAL2: -> PRINT WARNING -> RETURN  DON'T SWAP SAME VAL'S POINTLESS RUN TIME
+
+
+#SWAPPING NODE EXAMPLE
+def swap_nodes(input_list, val1, val2):
+  print(f'Swapping {val1} with {val2}')
+
+  node1_prev = None
+  node2_prev = None
+  node1 = input_list.head_node
+  node2 = input_list.head_node
+
+  if val1 == val2:
+    print("Elements are the same - no swap needed")
+    return
+
+  while node1 is not None:
+    if node1.get_value() == val1:
+      break
+    node1_prev = node1
+    node1 = node1.get_next_node()
+
+  while node2 is not None:
+    if node2.get_value() == val2:
+      break
+    node2_prev = node2
+    node2 = node2.get_next_node()
+
+  if (node1 is None or node2 is None):
+    print("Swap not possible - one or more element is not in the list")
+    return
+
+  if node1_prev is None:
+    input_list.head_node = node2
+  else:
+    node1_prev.set_next_node(node2)
+
+  if node2_prev is None:
+    input_list.head_node = node1
+  else:
+    node2_prev.set_next_node(node1)
+
+  temp = node1.get_next_node()
+  node1.set_next_node(node2.get_next_node())
+  node2.set_next_node(temp)
+
+
+ll = LinkedList.LinkedList()
+for i in range(10):
+  ll.insert_beginning(i)
+
+print(ll.stringify_list())
+swap_nodes(ll, 9, 5)
+print(ll.stringify_list())
