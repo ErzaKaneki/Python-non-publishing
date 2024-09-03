ff1fa7a731b1c810c4f16430c96b2f2923d719cb
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-03 14:53:21 -0500

    Added pop()

--- Stacks.py
+++ Stacks.py
@@ -11,3 +11,8 @@ class Stack:
         self.item = Node(value)
         self.item.set_next_node(self.top_item)
         self.top_item = self.item
+
+    def pop(self):
+        item_to_remove = self.top_item
+        self.top_item = item_to_remove.get_next_node()
+        return item_to_remove.get_value()
