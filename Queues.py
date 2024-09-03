9868a6a5ad19e0028c6164e3ce4cf1022646dc0d
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-03 12:58:45 -0500

    Added get_size, has_space, and is_empty.  Updated peek()

--- Queues.py
+++ Queues.py
@@ -1,8 +1,30 @@
 from Nodes import Node
 class Queue:
-    def __init__(self):
+    def __init__(self,max_size = None):
         self.head = None
         self.tail = None
+        self.max_size = max_size
+        self.size = 0
 
     def peek(self):
-        return self.head.get_value()
+        if self.is_empty() == True:
+            print("Nothing to see here!")
+        else:
+            return self.head.get_value()
+    
+    def get_size(self):
+        return self.size
+    
+    def has_space(self):
+        if self.max_size == None:
+            return True
+        elif self.max_size > self.get_size():
+            return True
+        else:
+            return False
+        
+    def is_empty(self):
+        if self.get_size() == 0:
+            return True
+        else:
+            return False
