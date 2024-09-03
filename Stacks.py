e267a83fa4f93359ead66e4baf119259f78bfd65
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-03 15:11:57 -0500

    updated peek() and pop()

--- Stacks.py
+++ Stacks.py
@@ -1,18 +1,28 @@
 from node import Node
 
 class Stack:
-    def __init__(self):
+    def __init__(self, limit = 1000):
         self.top_item = None
+        self.limit = limit
+        self.size = 0
     
     def peek(self):
-        return self.top_item.get_value()
+        if self.size > 0:
+            return self.top_item.get_value()
+        else:
+            print("This Stack is empty.")
     
     def push(self, value):
-        self.item = Node(value)
-        self.item.set_next_node(self.top_item)
-        self.top_item = self.item
+            self.item = Node(value)
+            self.item.set_next_node(self.top_item)
+            self.top_item = self.item
+
 
     def pop(self):
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.get_value()
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("This Stack is empty.")
