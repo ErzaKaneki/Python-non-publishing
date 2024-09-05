a36994439f7b0c566c9962f28bffd1cfac84c913
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-05 08:54:08 -0500

    Added values to a hash_map and tested its functionality.

--- Hash Maps.py
+++ Hash Maps.py
@@ -67,3 +67,13 @@ class HashMap:
                 return possible_return_value
             
             retrieval_collisions += 1
+        return
+
+hash_map = HashMap(15)
+hash_map.assign("gabbro", "igneous")
+hash_map.assign("sandstone", "sedimentary")
+hash_map.assign("gneiss", "metamorphic")
+
+print(hash_map.retrieve("gabbro"))
+print(hash_map.retrieve("sandstone"))
+print(hash_map.retrieve("gneiss"))
