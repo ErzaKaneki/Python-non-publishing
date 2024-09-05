2aae37cc0ac0ce0da80084a595df8565af94b270
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-05 09:34:37 -0500

    Commented out doubly, uncommented singly

--- Nodes.py
+++ Nodes.py
@@ -29,56 +29,56 @@
 
 
 #NODE IMPLEMENTATION
-# class Node:
-#     def __init__(self, value, next_node = None):
-#         self.value = value
-#         self.next_node = next_node
+class Node:
+    def __init__(self, value, next_node = None):
+        self.value = value
+        self.next_node = next_node
 
-#     def get_value(self):
-#         return self.value
+    def get_value(self):
+        return self.value
     
-#     def get_next_node(self):
-#         return self.next_node
+    def get_next_node(self):
+        return self.next_node
 
-#     def set_next_node(self, next_node):
-#         self.next_node = next_node
-#         return self.next_node
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+        return self.next_node
     
 
-# class LinkedList:
+class LinkedList:
 
-#     def __init__(self, value = None):
-#         self.head_node = Node(value)
+    def __init__(self, value = None):
+        self.head_node = Node(value)
 
-#     def get_head_node(self):
-#         return self.head_node
+    def get_head_node(self):
+        return self.head_node
     
-#     def insert_beginning(self, new_value):
-#         new_node = Node(new_value)
-#         new_node.set_next_node(self.head_node)
-#         self.head_node = new_node
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
     
-#     def stringify_list(self):
-#         end_string = ""
-#         current_node = self.get_head_node()
-#         while current_node:
-#             if current_node.get_value() != None:
-#                 end_string += str(current_node.get_value()) + "\n"
-#             current_node = current_node.get_next_node()
-#         return end_string
+    def stringify_list(self):
+        end_string = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                end_string += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return end_string
 
-#     def remove_node(self, value_to_remove):
-#         current_node = self.head_node
-#         if current_node.get_value() == value_to_remove:
-#             self.head_node = self.head_node.get_next_node()
-#             current_node = self.head_node
-#         else:
-#             while current_node:
-#                 previous_node = current_node
-#                 current_node = current_node.next_node
-#                 if current_node.get_value() == value_to_remove:
-#                     previous_node.next_node = current_node.next_node  #OR-> current_node.set_next_node(next_node.get_next_node()) <- TRANSPARENCY
-#                     current_node = None
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+        if current_node.get_value() == value_to_remove:
+            self.head_node = self.head_node.get_next_node()
+            current_node = self.head_node
+        else:
+            while current_node:
+                previous_node = current_node
+                current_node = current_node.next_node
+                if current_node.get_value() == value_to_remove:
+                    previous_node.next_node = current_node.next_node  #OR-> current_node.set_next_node(next_node.get_next_node()) <- TRANSPARENCY
+                    current_node = None
 
 # LL = LinkedList()
 # LL.insert_beginning(5)
@@ -218,144 +218,144 @@
 
 
 #NODE CLASS AND CONSTRUCTOR DOUBLY LINKED LIST
-class Node:
-  def __init__(self, value, next_node=None, prev_node=None):
-    self.value = value
-    self.next_node = next_node
-    self.prev_node = prev_node
+# class Node:
+#   def __init__(self, value, next_node=None, prev_node=None):
+#     self.value = value
+#     self.next_node = next_node
+#     self.prev_node = prev_node
     
-  def set_next_node(self, next_node):
-    self.next_node = next_node
+#   def set_next_node(self, next_node):
+#     self.next_node = next_node
     
-  def get_next_node(self):
-    return self.next_node
+#   def get_next_node(self):
+#     return self.next_node
 
-  def set_prev_node(self, prev_node):
-    self.prev_node = prev_node
+#   def set_prev_node(self, prev_node):
+#     self.prev_node = prev_node
     
-  def get_prev_node(self):
-    return self.prev_node
+#   def get_prev_node(self):
+#     return self.prev_node
   
-  def get_value(self):
-    return self.value
+#   def get_value(self):
+#     return self.value
     
 
-class DoublyLinkedList:
-  def __init__(self, head_node = None, tail_node = None):
-    self.head_node = head_node
-    self.tail_node = tail_node
+# class DoublyLinkedList:
+#   def __init__(self, head_node = None, tail_node = None):
+#     self.head_node = head_node
+#     self.tail_node = tail_node
 
-  def add_to_head(self, new_value):
-    new_head = Node(new_value)
-    current_head = self.head_node
+#   def add_to_head(self, new_value):
+#     new_head = Node(new_value)
+#     current_head = self.head_node
     
-    if current_head != None:
-      current_head.set_prev_node(new_head)
-      new_head.set_next_node(current_head)
+#     if current_head != None:
+#       current_head.set_prev_node(new_head)
+#       new_head.set_next_node(current_head)
 
-    self.head_node = new_head
+#     self.head_node = new_head
 
-    if self.tail_node == None:
-      self.tail_node = new_head
+#     if self.tail_node == None:
+#       self.tail_node = new_head
 
-  def add_to_tail(self, new_value):
-    new_tail = Node(new_value)
-    current_tail = self.tail_node
+#   def add_to_tail(self, new_value):
+#     new_tail = Node(new_value)
+#     current_tail = self.tail_node
 
-    if current_tail != None:
-      current_tail.set_next_node(new_tail)
-      new_tail.set_prev_node(current_tail)
+#     if current_tail != None:
+#       current_tail.set_next_node(new_tail)
+#       new_tail.set_prev_node(current_tail)
     
-    self.tail_node = new_tail
+#     self.tail_node = new_tail
 
-    if self.head_node == None:
-      self.head_node = new_tail
+#     if self.head_node == None:
+#       self.head_node = new_tail
 
 #REMOVING THE HEAD (DOUBLY)
 
-  def remove_head(self):
+#   def remove_head(self):
     
-    removed_head = self.head_node
+#     removed_head = self.head_node
     
-    if removed_head.value == None:
-      return None
+#     if removed_head.value == None:
+#       return None
     
-    self.head_node = removed_head.get_next_node()
+#     self.head_node = removed_head.get_next_node()
     
-    if self.head_node != None:
-      self.head_node.prev_node = None
+#     if self.head_node != None:
+#       self.head_node.prev_node = None
 
-    if removed_head == self.tail_node:
-      self.remove_tail()
+#     if removed_head == self.tail_node:
+#       self.remove_tail()
 
-    return removed_head.get_value()
+#     return removed_head.get_value()
   
-#REMOVING THE TAIL
+# #REMOVING THE TAIL
 
-  def remove_tail(self):
+#   def remove_tail(self):
     
-    removed_tail = self.tail_node
+#     removed_tail = self.tail_node
     
-    if removed_tail.value == None:
-      return None
+#     if removed_tail.value == None:
+#       return None
     
-    self.tail_node = removed_tail.get_prev_node()
+#     self.tail_node = removed_tail.get_prev_node()
     
-    if self.tail_node != None:
-      self.tail_node.next_node = None
+#     if self.tail_node != None:
+#       self.tail_node.next_node = None
 
-    if removed_tail == self.head_node:
-      self.remove_head()
+#     if removed_tail == self.head_node:
+#       self.remove_head()
 
-    return removed_tail.get_value()
+#     return removed_tail.get_value()
   
-#REMOVING BY VALUE I
+# #REMOVING BY VALUE I
 
-  def remove_by_value(self, value_to_remove):
+#   def remove_by_value(self, value_to_remove):
 
-    node_to_remove = None
+#     node_to_remove = None
 
-    current_node = self.head_node
+#     current_node = self.head_node
 
-    while current_node != None:
-      if current_node.get_value() == value_to_remove:
-        node_to_remove = current_node
-        break
+#     while current_node != None:
+#       if current_node.get_value() == value_to_remove:
+#         node_to_remove = current_node
+#         break
 
-      current_node = current_node.get_next_node()
+#       current_node = current_node.get_next_node()
 
-    if node_to_remove == None:
-      return None  
+#     if node_to_remove == None:
+#       return None  
     
-#REMOVING BY VALUE II
+# #REMOVING BY VALUE II
 
-    if node_to_remove == self.head_node:
-      self.remove_head()
-    elif node_to_remove == self.tail_node:
-      self.remove_tail()
-    else:
-      next_node = node_to_remove.get_next_node()
-      prev_node = node_to_remove.get_prev_node()
-      next_node.set_prev_node(prev_node)
-      prev_node.set_next_node(next_node)
+#     if node_to_remove == self.head_node:
+#       self.remove_head()
+#     elif node_to_remove == self.tail_node:
+#       self.remove_tail()
+#     else:
+#       next_node = node_to_remove.get_next_node()
+#       prev_node = node_to_remove.get_prev_node()
+#       next_node.set_prev_node(prev_node)
+#       prev_node.set_next_node(next_node)
     
-    return node_to_remove
+#     return node_to_remove
   
-#USING THE LIST
+# #USING THE LIST
 
-  def stringify_list(self):
+#   def stringify_list(self):
     
-    string_list = ""
-    current_node = self.head_node
+#     string_list = ""
+#     current_node = self.head_node
     
-    while current_node:
+#     while current_node:
       
-      if current_node.get_value() != None:
-        string_list += str(current_node.get_value()) + "\n"
+#       if current_node.get_value() != None:
+#         string_list += str(current_node.get_value()) + "\n"
       
-      current_node = current_node.get_next_node()
+#       current_node = current_node.get_next_node()
     
-    return string_list
+#     return string_list
   
 # subway = DoublyLinkedList()  
 # subway.add_to_head("Times Square")
