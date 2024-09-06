689932b5b6a5b57eab8bc7b52b8dd64a0384b3e1
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-06 14:59:30 -0500

    Added alternate sum_to_one() Added factorial()

--- recursion.py
+++ recursion.py
@@ -1,18 +1,38 @@
-def sum_to_one(n):
-    result = 1
-    call_stack = []
+# def sum_to_one(n):
+#     result = 1
+#     call_stack = []
 
-    while n != 1:
-        execution_context = {"n_value": n}
-        call_stack.append(execution_context)
-        n -= 1
-        print(call_stack)
-    print("BASE CASE REACHED")
-    while call_stack:
-        return_value = call_stack.pop()
-        print("\nAdding return_value {0} to result {1}".format(return_value["n_value"], result))
-        result += return_value["n_value"]
-        print(call_stack)
-    return result, call_stack
+#     while n != 1:
+#         execution_context = {"n_value": n}
+#         call_stack.append(execution_context)
+#         n -= 1
+#         print(call_stack)
+#     print("BASE CASE REACHED")
+#     while call_stack:
+#         return_value = call_stack.pop()
+#         print("\nAdding return_value {0} to result {1}".format(return_value["n_value"], result))
+#         result += return_value["n_value"]
+#         print(call_stack)
+#     return result, call_stack
 
-sum_to_one(4)
+# sum_to_one(4)
+
+
+# def sum_to_one(n):
+#     if n == 1:
+#         print("Recursing with input: {0}".format(n))
+#         return n
+#     elif n != 1:
+#         print("Recursing with input: {0}".format(n))
+#         return n + sum_to_one(n - 1)
+    
+# print(sum_to_one(7))
+
+
+def factorial(n):
+    if n == 1:
+        return n
+    elif n != 1:
+        return n * factorial(n - 1)
+    
+print(factorial(12)) #found that this only works up to 998 XD  999 and up will throw a RecursionError
