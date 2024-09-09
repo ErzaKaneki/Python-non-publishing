1cfd20cb5080cecdf61d597c62b015d3f7f350cf
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-09 16:42:01 -0500

    testing linear_search()

--- Linear Search.py
+++ Linear Search.py
@@ -1,9 +1,29 @@
-def linear_search(search_list, target_value):
-    for i in range(len(search_list)):
-        print(search_list[i])
-        if search_list[i] == target_value:
-            return i
-    raise ValueError("{0} not in list".format(target_value))
+# def linear_search(search_list, target_value):
+#     for i in range(len(search_list)):
+#         print(search_list[i])
+#         if search_list[i] == target_value:
+#             return i
+#     raise ValueError("{0} not in list".format(target_value))
 
-values = [54, 22, 46, 99]
-print(linear_search(values, 22))
+# values = [54, 22, 46, 99]
+# print(linear_search(values, 22))
+
+
+# number_list = [ 10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
+# target_number = 33
+
+# def linear_search(search_list, target_value):
+#   for idx in range(len(search_list)):
+#     if search_list[idx] == target_value:
+#       return idx
+#   raise ValueError("{0} not in list".format(target_value))
+
+
+
+# try:
+#   # Call the function below...
+#     print(linear_search(number_list, 100))
+# except ValueError as error_message:
+#   print("{0}".format(error_message))
+
+
