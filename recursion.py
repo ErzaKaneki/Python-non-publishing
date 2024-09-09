4ba5ab7b0d607494f548bfe1f0925d4fd3d91c47
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-09 08:53:57 -0500

    fibonacci() 2^N lesson

--- recursion.py
+++ recursion.py
@@ -78,3 +78,15 @@
 # print(flatten(planets))
 
 
+def fibonacci(n):
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(5))
+print(fibonacci(10))
+print(fibonacci(7))
+
+fibonacci_runtime = "2^N"
