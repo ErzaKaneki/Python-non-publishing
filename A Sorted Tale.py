022a374ec697fdb7447676fd01a1ff5a50a80ffa
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-10 16:04:19 -0500

    sort comparisons: by_total_length() by_title_ascending() by_author_ascending() tested: sort_1 sort_2 quicksort() altered bubble_sort() altered quicksort() to handle alternate sorting types

--- A Sorted Tale.py
+++ A Sorted Tale.py
@@ -2,5 +2,28 @@ import utils
 import sorts
 
 bookshelf = utils.load_books("books_small.csv")
+long_bookshelf = utils.load_books("books_large.csv")
+bookshelf_v1 = bookshelf.copy()
+bookshelf_v2 = bookshelf.copy()
 
-print(bookshelf)
+def by_title_ascending(book_a, book_b):
+    return book_a['title_lower'] > book_b['title_lower']
+
+def by_author_ascending(book_a, book_b):
+    return book_a['author_lower'] > book_b['author_lower']\
+    
+def by_total_length(book_a, book_b):
+    return len(book_a['author']) + len(book_a['title']) > len(book_b['author']) + len(book_b['title'])
+
+sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+for book in sort_1:
+    print(book['title'])
+
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
+for book in sort_2:
+    print(book['author'])
+
+print("\nQuicksort:")
+sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2) - 1, by_author_ascending)
+for book in bookshelf_v2:
+    print(book['author'])
