1d9416bad81660adc02c778708aa72262b256fa2
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 15:54:16 -0500

    checked if root.value is target we are searching for

--- Trees/dfs.py
+++ Trees/dfs.py
@@ -1,3 +1,8 @@
 from treenode import TreeNode, sample_root_node, print_path, print_tree
 
 print_tree(sample_root_node)
+
+def dfs(root, target):
+    if root.value == target:
+        return root
+    return None
