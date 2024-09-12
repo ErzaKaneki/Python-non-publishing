cf9635e01be13e8be15e7874b4cad405ef86594c
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 16:07:51 -0500

    changed print for dfs to variable and printed the variable instead.

--- Trees/dfs.py
+++ Trees/dfs.py
@@ -5,10 +5,14 @@ print_tree(sample_root_node)
 def dfs(root, target):
     if root.value == target:
         return root
+    
     for child in root.children:
         node_found = dfs(child, target)
+        
         if node_found != None:
             return node_found
+   
     return None
 
-print(dfs(sample_root_node, "F"))
+search = dfs(sample_root_node, "F")
+print(search)
