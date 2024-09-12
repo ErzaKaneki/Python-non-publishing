04fe8ecdf46231b584fd446948ca2e56c4eaf819
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 08:47:11 -0500

    Updated remove child with list comprehension. removed old varients

--- treenode.py
+++ treenode.py
@@ -1,15 +1,13 @@
 class TreeNode:
-  def __init__(self, value):
-    self.value = value
-    self.children = []
+    def __init__(self, value):
+        self.value = value
+        self.children = []
 
-  def add_child(self, child_node):
-    print("Adding " + child_node.value)
-    self.children.append(child_node)
+    def add_child(self, child_node):
+        print("Adding " + child_node.value)
+        self.children.append(child_node)
 
-root = TreeNode("I am Root")
-child = TreeNode("A wee sappling")
-bad_seed = TreeNode("Root Rot!")
+    def remove_child(self, child_node):
+        print("Removing " + child_node.value + " from " + self.value)
+        self.children = [child for child in self.children if child != child_node]
 
-root.add_child(child)
-root.add_child(bad_seed)
