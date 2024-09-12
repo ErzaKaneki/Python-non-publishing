2f95cf50c6e6c7ab97c05cfb3e92a8dd0e95e175
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 09:19:08 -0500

    Implemented a traverse() that goes to every node below called node

--- treenode.py
+++ treenode.py
@@ -11,3 +11,23 @@ class TreeNode:
         print("Removing " + child_node.value + " from " + self.value)
         self.children = [child for child in self.children if child != child_node]
 
+    def traverse(self):
+        print("Traversing...")
+        nodes_to_visit = [self]
+        while len(nodes_to_visit) > 0:
+            current_node = nodes_to_visit.pop()
+            print(current_node.value)
+            nodes_to_visit += current_node.children
+
+
+
+root = TreeNode("CEO")
+first_child = TreeNode("Vice-President")
+second_child = TreeNode("Head of Marketing")
+third_child = TreeNode("Marketing Assistant")
+
+root.add_child(first_child)
+root.add_child(second_child)
+second_child.add_child(third_child)
+
+root.traverse()
