f9308484399bd282459bad9922252adbf5a67cef
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 08:25:40 -0500

    Added "bad_seed" variant to "root" parent

--- treenode.py
+++ treenode.py
@@ -1,13 +1,15 @@
 class TreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
+  def __init__(self, value):
+    self.value = value
+    self.children = []
 
-    def add_child(self, child_node):
-        print("Adding " + child_node.value)
-        self.children.append(child_node)
+  def add_child(self, child_node):
+    print("Adding " + child_node.value)
+    self.children.append(child_node)
 
 root = TreeNode("I am Root")
 child = TreeNode("A wee sappling")
+bad_seed = TreeNode("Root Rot!")
 
 root.add_child(child)
+root.add_child(bad_seed)
