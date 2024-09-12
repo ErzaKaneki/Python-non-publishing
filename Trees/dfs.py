9b8d53b8fd8c87c8cc69ed52d92a98bd6f2d0afc
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-12 15:47:57 -0500

    created dfs.py for Depth first search modified treenode.py for this exercise

---
+++ Trees/dfs.py
@@ -0,0 +1,3 @@
+from treenode import TreeNode, sample_root_node, print_path, print_tree
+
+print_tree(sample_root_node)
--- Trees/treenode.py
+++ Trees/treenode.py
@@ -1,33 +1,70 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, value):
-        self.value = value
-        self.children = []
+        self.value = value # data
+        self.children = [] # references to other nodes
+        
+    def __repr__(self):
+        return self.value
 
     def add_child(self, child_node):
+        # creates parent-child relationship
         print("Adding " + child_node.value)
-        self.children.append(child_node)
-
+        self.children.append(child_node) 
+        
     def remove_child(self, child_node):
+        # removes parent-child relationship
         print("Removing " + child_node.value + " from " + self.value)
-        self.children = [child for child in self.children if child != child_node]
+        self.children = [child for child in self.children 
+                        if child is not child_node]
 
     def traverse(self):
-        print("Traversing...")
+        # moves through each node referenced from self downwards
         nodes_to_visit = [self]
         while len(nodes_to_visit) > 0:
             current_node = nodes_to_visit.pop()
             print(current_node.value)
             nodes_to_visit += current_node.children
 
+      
+      
+def print_tree(root):
+    stack = deque()
+    stack.append([root, 0])
+    level_str = "\n"
+    while len(stack) > 0:
+        node, level = stack.pop()
+        
+        if level > 0:
+            level_str += "| "*(level-1)+ "|-"
+        level_str += str(node.value)
+        level_str += "\n"
+        level+=1
+        for child in node.children:
+            stack.append([child, level])
 
+    print(level_str)
+      
+def print_path(path):
+  # If path is None, no path was found
+    if path == None:
+        print("No paths found!")
 
-root = TreeNode("CEO")
-first_child = TreeNode("Vice-President")
-second_child = TreeNode("Head of Marketing")
-third_child = TreeNode("Marketing Assistant")
+    # If a path was found, print path
+    else:  
+        print("Path found:")
+        for node in path:
+            print(node.value)
 
-root.add_child(first_child)
-root.add_child(second_child)
-second_child.add_child(third_child)
-
-root.traverse()
+      
+sample_root_node = TreeNode("A")
+two = TreeNode("B")
+three = TreeNode("C")
+sample_root_node.children = [three, two]
+four = TreeNode("D")
+five = TreeNode("E")
+six = TreeNode("F")
+seven = TreeNode("G")
+two.children = [five, four]
+three.children = [seven, six]
