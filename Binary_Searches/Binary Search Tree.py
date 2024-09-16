83b98812bd2240ac52a3427e755bfe52bd728265
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-16 11:55:27 -0500

    Added: get_node_by_value() retrieval method.

--- Binary_Searches/Binary Search Tree.py
+++ Binary_Searches/Binary Search Tree.py
@@ -18,6 +18,16 @@ class BinarySearchTree:
                 print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
             else:
                 self.right.insert(value)
+                
+    def get_node_by_value(self, value):
+        if value == self.value:
+            return self
+        elif value < self.value and self.left:
+            return self.left.get_node_by_value(value)
+        elif value > self.value and self.right:
+            return self.right.get_node_by_value(value)
+        else:
+            return None
 
 
 root = BinarySearchTree(100)
@@ -26,3 +36,7 @@ root.insert(125)
 root.insert(75)
 root.insert(25)
 
+run1 = root.get_node_by_value(75)
+print(run1.value)
+run2 = root.get_node_by_value(55)
+print(run2)
