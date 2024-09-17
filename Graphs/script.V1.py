9d369df18eb10388a6e3e7280f57a26d7a61465b
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-17 09:44:31 -0500

    Edited: script.V1.py graph.py Edits: set up edges for railway add_edge()

---
+++ Graphs/script.V1.py
@@ -0,0 +1,14 @@
+from graph import Graph
+from vertex import Vertex
+
+railway = Graph()
+station_one = Vertex("Ballahoo")
+station_two = Vertex("Penn")
+
+railway.add_vertex(station_one)
+railway.add_vertex(station_two)
+
+railway.add_edge(station_one, station_two)
+
+print("Edges for {0}: {1}".format(station_one.value, station_one.get_edges()))
+print("Edges for {0}: {1}".format(station_two.value, station_two.get_edges()))
