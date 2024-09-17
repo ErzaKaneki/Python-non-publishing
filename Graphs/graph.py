518e99ece5ed3633fff7324525fc9caf32568777
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-17 12:18:55 -0500

    Changed test data in script.V1.py Updated graph.py to be for efficiant by not running loops around nodes endlessly.

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -12,8 +12,17 @@ class Graph:
             self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
     def find_path(self, start_vertex, end_vertex):
-        print("Searching for route from {0} to {1}".format(start_vertex, end_vertex))
         start = [start_vertex]
+        seen = {}
         while len(start) > 0:
             current_vertex = start.pop(0)
+            seen[current_vertex] = True
             print("Visiting " + current_vertex)
+            if current_vertex == end_vertex:
+                return True
+            else:
+                vertex = self.graph_dict[current_vertex]
+                next_vertices = vertex.get_edges()
+                next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+                start.extend(next_vertices)
+        return False
