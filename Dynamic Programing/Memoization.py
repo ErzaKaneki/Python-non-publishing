255ece256c1729ad353f96d12f9e115932cca6ed
Erza Kaneki <nevin.kadlec@gmail.com>
2024-11-04 10:38:40 -0600

    updated code

--- Dynamic Programing/Memoization.py
+++ Dynamic Programing/Memoization.py
@@ -2,17 +2,14 @@ memo = {}
 
 def fibonacci(num):
     answer = None
-
     if num in memo:
-        return memo[num]
+        answer = memo[num]
+    elif num == 0 or num == 1:
+        answer = num
     else:
-        if num == 0:
-            return 0
-        elif num == 1:
-            return 1
-        else:
-            memo[num] = fibonacci(num -1) + fibonacci(num - 2)
-            return memo[num]
+        answer = fibonacci(num - 1) + fibonacci(num - 2)
+    memo[num] = answer        
+    return answer
         
 print(fibonacci(20))
-print(fibonacci(200))        
+print(fibonacci(100))        
